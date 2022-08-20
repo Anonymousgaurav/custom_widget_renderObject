b762f5c12941bbd1dca38bda9ad4194df0b6e893
Gaurav Kumar <gauravswarankar1@gmail.com>
2022-08-20 20:14:16 +0530

    custom widget SingleChildRenderObjectWidget done !!!

---
+++ android/app/src/main/kotlin/com/gaurav/custom_widget_renderobject/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gaurav.custom_widget_renderobject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presentation/single_render_object.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class MyRenderPhysicalShapeDemo extends StatefulWidget {
+
+  const MyRenderPhysicalShapeDemo({Key? key}): super(key: key);
+
+  @override
+  MyRenderPhysicalShapeState createState() {
+    return MyRenderPhysicalShapeState();
+  }
+}
+
+class MyRenderPhysicalShapeState extends State<MyRenderPhysicalShapeDemo> {
+  double _elevation = 0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('MyRenderPhysicalShapeDemo')),
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Slider(
+                value: _elevation,
+                onChanged: (value) => setState(() => _elevation = value),
+              ),
+              MyPhysicalShape(
+                elevation: _elevation,
+                child: const SizedBox(
+                  width: 50,
+                  height: 50,
+                ),
+              )
+            ]));
+  }
+}
+
+class MyPhysicalShape extends SingleChildRenderObjectWidget {
+  final _elevation ;
+
+  const MyPhysicalShape({required double elevation,required Widget child,Key? key})
+      : _elevation = elevation,
+        super(child: child,key: key);
+  @override
+  RenderPhysicalShape createRenderObject(BuildContext context) {
+    return RenderPhysicalShape(
+      clipBehavior: Clip.none,
+      color: const Color(0x61000000),
+      clipper: ShapeBorderClipper(
+        shape: const CircleBorder(),
+        textDirection: Directionality.of(context),
+      ),
+      shadowColor: Colors.black,
+      elevation: _elevation,
+    );
+  }
+
+  @override
+  void updateRenderObject(BuildContext context, RenderPhysicalShape renderObject) {
+    renderObject.elevation = _elevation;
+  }
+}
